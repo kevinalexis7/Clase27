83296c2f2bdf30317fd680775d5aaa8e108f6dd7
Kevinalexis7 <kevinalex.espinola09@gmail.com>
2023-09-18 10:36:49 -0300

    front de calculadora

---
+++ app.js
@@ -0,0 +1,26 @@
+//todo: hacer el front de la calculadora
+const process = require('process');
+
+const {argv} = process;
+
+function calculadora(comando) {
+    switch (comando) {
+        case 'sumar':
+            
+            break;
+        case 'restar':
+            
+            break;
+        case 'multiplicar':
+            
+            break;
+        case 'dividir':
+            
+            break;
+    
+        default:
+            return "comando no valido";
+    }
+}
+
+console.log(calculadora(argv[2], argv[3], argv[4]))
